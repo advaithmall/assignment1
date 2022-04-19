73dd72b1cef26cd394b8e5fa90b29ec1723c64fa
Advaith Malladi <98263352+advaithmall@users.noreply.github.com>
2022-04-19 22:55:54 +0530

    Update q5.sh

--- q5.sh
+++ q5.sh
@@ -11,4 +11,6 @@ word2=$word1
 echo $word1 | tr 'a-zA-Z' 'b-zA-Za'
 N=1
 let M=(strlen)/2
-echo "word2" | cut -c$N-$M
+#echo "$word2" | cut -c$N-$M
+echo "$word2" | cut -c$(( $M+1 ))-$strlen
+echo "$word" | cut -c$(( $M+1 ))-$strlen
